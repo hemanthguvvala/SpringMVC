f0221c34588854d363c8edc1313dc794f1cd6154
Hemanth Kumar <khemanth00321@gmail.com>
2021-03-30 09:55:55 +0530

    initial commit

---
+++ SpringMVCProject/src/main/java/com/hkgroups/FirstController.java
@@ -0,0 +1,15 @@
+package com.hkgroups;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class FirstController {
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String Welcome() {
+		return "welcome";
+	}
+
+}
